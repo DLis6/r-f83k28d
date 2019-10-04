48ac4a52f1af0babb747ac34a13ef036b4ac7cdc
David <54381677+DLis6@users.noreply.github.com>
2019-10-03 19:26:08 -0500

    Contador

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    count: 0
+  };
+  buttonClick = () => {
+    this.setState(({ count }) => ({
+      count: count + 1
+    }));
+  };
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.count}</span>
+        <button id="inc" onClick={this.buttonClick}>Incrementa</button>
       </div>
     );
   }
